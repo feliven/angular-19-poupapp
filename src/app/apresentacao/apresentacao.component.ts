3d35b3abffc498288f6e55e033d301a1caacaa95
FVS <feliven@gmail.com>
2025-10-31 13:35:25 -0300

    first commit

---
+++ src/app/apresentacao/apresentacao.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apresentacao',
+  imports: [],
+  templateUrl: './apresentacao.component.html',
+  styleUrl: './apresentacao.component.css',
+})
+export class ApresentacaoComponent {}
