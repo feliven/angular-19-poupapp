5ff01042fd0197445ee2a37489c97dcb8c81f9f1
FVS <feliven@gmail.com>
2025-11-04 13:23:38 -0300

    troca afterRender por effect para exibir ElementRef no console só quando página mudar

--- src/app/compartilhados/destaque-valor-numerico.directive.ts
+++ src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -1,4 +1,10 @@
-import { afterRender, Directive, ElementRef, input } from '@angular/core';
+import {
+  afterRender,
+  Directive,
+  effect,
+  ElementRef,
+  input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDestaqueValorNumerico]',
@@ -11,7 +17,8 @@ export class DestaqueValorNumericoDirective {
   corValorNegativo = input('var(--destaque-despesa)');
 
   constructor(elemento: ElementRef<HTMLElement>) {
-    afterRender(() => {
+    // using effect() so it runs when signals change (unlike afterRender())
+    effect(() => {
       if (this.appDestaqueValorNumerico() > 0) {
         elemento.nativeElement.style.color = this.corValorPositivo();
       } else if (this.appDestaqueValorNumerico() < 0) {
